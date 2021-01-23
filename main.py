ae1459da293c2d9eede708bb2e15d85e060a63fb
c-kip <callumkipin@gmail.com>
2021-01-22 22:08:25 -0500

    Reorganized command calling process

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ load_dotenv(os.path.join(project_folder, '.env'))
 
 client = discord.Client()
 
+async def process_command(message):
+    parameters = message.content.split(' ')
+
+    if (len(parameters) > 0):
+        if (parameters[0] == '$hello'):
+            await message.channel.send('Hello!')
+        elif (parameters[0] == '$stop'):
+            await message.channel.send('Buy-bye!')
+            await client.logout()
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -15,10 +25,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    if message.content == '$stop': 
-        await client.logout()
+    if message.content.startswith('$'):
+        await process_command(message)
         
 client.run(os.getenv('TOKEN'))
