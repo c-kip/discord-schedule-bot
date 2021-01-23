43b58413a8565c838329d31e4a917b7641ef152b
c-kip <callumkipin@gmail.com>
2021-01-23 13:39:09 -0500

    Finished make_meeting(), and revamped show_meetings()

--- main.py
+++ main.py
@@ -11,36 +11,67 @@ meetings = []
 
 async def make_meeting(parameters):
     #Name parameter
-    name = parameters[0]
+    name = "Undefined"
+    if (len(parameters) >= 1):
+        name = parameters[0]
 
-    #Time parameter HH:MM (24HR)
-    meeting_time = datetime.datetime.now().time #Default is now
-    if (len(parameters[1]) == 5):
-        meeting_time = datetime.time(int(parameters[1][:2]), int(parameters[1][3:]))
-
-    #Date parameter DD/MM/YYYY (if year is omitted, assumed the current)
+    #Default values
+    meeting_time = datetime.datetime.now().time() #Default time is now
     meeting_date = datetime.date.today() #If the date is omitted, assume today
-    if (len(parameters[2]) == 5):
-        meeting_date = datetime.date(meeting_date.year, int(parameters[2][3:]), int(parameters[2][:2]))
-    elif (len(parameters[2]) == 10):
-        meeting_date = datetime.date(int(parameters[2][6:]), int(parameters[2][3:5]), int(parameters[2][:2]))
+    participants = []
+    desc = ""
+    copy_desc = False
+    auto_remind = False #Default is no auto_remind
 
-    #Participants parameter - all the @ users
-    #TODO
+    #Loop through the remaining parameters
+    for param in parameters[1:]:
+        print(param)
 
-    #Desc parameter - string (in quotes)
-    #TODO
+        #Time parameter HH:MM (24HR)
+        if (len(param) == 5 and param[2] == ':'):
+            meeting_time = datetime.time(int(param[:2]), int(param[3:]))
 
-    #Autoremind parameter - TRUE or FALSE
-    #TODO
+        #Date parameter DD/MM/YYYY (if year is omitted, assumed the current)
+        if (len(param) == 5 and param[2] == '/'):
+            meeting_date = datetime.date(meeting_date.year, int(param[3:]), int(param[:2]))
+        elif (len(param) == 10 and param[2] == '/' and param[5] == '/'):
+            meeting_date = datetime.date(int(param[6:]), int(param[3:5]), int(param[:2]))
 
-    meetings.append(schedule.Meeting(name, meeting_time, meeting_date))
+        #Participants parameter - all the @ users
+        if (param[:3] == '<@!'):
+            participants.append(await client.guilds[0].fetch_member(int(param[3:-1]))) #Add the user as a 'member' object
+
+        #Desc parameter start - string (in quotes)
+        if (param.startswith("'")):
+            copy_desc = True
+            param = param[1:] #Remove the quotation
+        
+        #End of desc
+        if (param[-1] == "'"):
+            copy_desc = False
+            desc += param[:-1] #Remove the quotation
+
+        #Add part of the desc
+        if (copy_desc):
+            desc += param + ' '
+
+        #Autoremind parameter - TRUE or FALSE
+        if (param.lower() == "true"):
+            auto_remind = True
+
+    meetings.append(schedule.Meeting(name, meeting_time, meeting_date, participants, desc, auto_remind))
 
 async def show_meetings(message):
     for meeting in meetings:
+        participant_names = ""
+        for participant in meeting.getParticipants():
+            participant_names += participant.display_name + ", "
         await message.channel.send(meeting.getName() + "\n" + 
               str(meeting.getTime().hour).zfill(2) + ":" + str(meeting.getTime().minute).zfill(2) + " " + str(meeting.getDate().day).zfill(2) + 
-              "/" + str(meeting.getDate().month).zfill(2) + "/" + str(meeting.getDate().year).zfill(2))
+              "/" + str(meeting.getDate().month).zfill(2) + "/" + str(meeting.getDate().year).zfill(2) + "\n" +
+              "Participants: " + participant_names + "\n" + 
+              "Desc: " + meeting.getDesc() + "\n" +
+              "Auto-Remind: " + str(meeting.getAutoRemind()))
 
 async def delete_meeting(message):
     for meeting in meetings:
