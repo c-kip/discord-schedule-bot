4d8f48675b81a260717906f37ff3f43638319f85
c-kip <callumkipin@gmail.com>
2021-01-23 15:41:25 -0500

    Added 'edit' command for meetings, and reorganized make_meeting, created parse_meeting_info and edit_meeting

--- main.py
+++ main.py
@@ -38,28 +38,16 @@ async def dm_missing(message):
             else:
                 await person.send('u in meeting :)')
     
-async def make_meeting(parameters):
-    #Name parameter
-    name = "Undefined"
-    if (len(parameters) >= 1):
-        for meet in meetings:
-            if (meet.getName() == parameters[0]):
-                return "A meeting already uses that name!"
-        name = parameters[0]
-    else:
-        return "No parameters given!"
-
-    #Default values
-    meeting_time = datetime.datetime.now().time() #Default time is now
-    meeting_date = datetime.date.today() #If the date is omitted, assume today
+async def parse_meeting_info(parameters):
+    meeting_time = None
+    meeting_date = None
     participants = []
-    desc = ""
+    desc = ''
+    auto_remind = None
     copy_desc = False
-    auto_remind = False #Default is no auto_remind
 
     #Loop through the remaining parameters
-    for param in parameters[1:]:
-        print(param)
+    for param in parameters:
 
         #Time parameter HH:MM (24HR)
         if (len(param) == 5 and param[2] == ':'):
@@ -79,6 +67,7 @@ async def make_meeting(parameters):
         if (param.startswith("'")):
             copy_desc = True
             param = param[1:] #Remove the quotation
+            desc = ""
         
         #End of desc
         if (param[-1] == "'"):
@@ -92,10 +81,64 @@ async def make_meeting(parameters):
         #Autoremind parameter - TRUE or FALSE
         if (param.lower() == "true"):
             auto_remind = True
+        elif (param.lower() == 'false'):
+            auto_remind = False
+    
+    return meeting_time, meeting_date, participants, desc, auto_remind
+
+async def make_meeting(parameters):
+    #Name parameter
+    name = "Undefined"
+    if (len(parameters) >= 1):
+        for meet in meetings:
+            if (meet.getName() == parameters[0]):
+                return "A meeting already uses that name!"
+        name = parameters[0]
+    else:
+        return "No parameters given!"
+
+    meeting_time, meeting_date, participants, desc, auto_remind = await parse_meeting_info(parameters[1:])
+
+    if (meeting_time == None):
+        meeting_time = datetime.datetime.now().time() #Default time is now
+    if (meeting_date == None):
+        meeting_date = datetime.date.today() #If the date is omitted, assume today
+    if (auto_remind == None):
+        auto_remind = False #Default is no auto_remind
 
     meetings.append(schedule.Meeting(name, meeting_time, meeting_date, participants, desc, auto_remind))
     
     return None
+
+#Updates the parameters for the given meetings
+async def update_meeting(message, parameters):
+    found = -1
+    for i in range(len(meetings)):
+        if (meetings[i].getName() == parameters[0]):
+            found = i
+    
+    if (found == -1):
+        await message.channel.send("No meeting of name '{}' found.".format(parameters[0]))
+        return False
+
+    meeting_time, meeting_date, participants, desc, auto_remind = await parse_meeting_info(parameters[1:])
+
+    #Set any changed values
+    if (meeting_time != None):
+        meetings[found].setTime(meeting_time)
+    if (meeting_date != None):
+        meetings[found].setDate(meeting_date)
+    if (participants != []): #Not NONE
+        for person in participants:
+            if (not(meetings[found].addParticipant(person))):
+                try:
+                    await message.channel.send("{} has already signed up for {}".format(person, meetings[found].getName()))
+                except:
+                    print("Error, likelyl no message given (meeting creation issue).")
+    if (desc != ''):
+        meetings[found].setDesc(desc)
+    if (auto_remind != None):
+        meetings[found].setAutoRemind(auto_remind)
 
 async def show_meetings(message):
     for meeting in meetings:
@@ -125,6 +168,8 @@ async def process_command(message):
             meetings[-1].setMessage(message)
         elif (parameters[0] == 'show_meetings'):
             await show_meetings(message)
+        elif (parameters[0] == 'edit'):
+            await update_meeting(message, parameters[1:])
         elif (parameters[0] == 'missing'):
             await dm_missing(message)
             # await message.channel.send(message.author)
@@ -138,7 +183,7 @@ async def on_reaction_add(reaction, user):
         for meeting in meetings:
             if (reaction.message == meeting.getMessage()):
                 if (meeting.addParticipant(user)):
-                    await channel.send("{} has added successfully signed up for {}".format(user.name, meeting.name))
+                    await channel.send("{} has successfully signed up for {}".format(user.name, meeting.name))
                 else:
                     await channel.send("{} has already signed up for {}".format(user.name, meeting.name))
 
