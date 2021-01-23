120511e8d6bd87d1ec533404152a4c1e269a0f20
c-kip <callumkipin@gmail.com>
2021-01-23 11:31:51 -0500

    Added time and date to meeting creation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import schedule
+import datetime
 from dotenv import load_dotenv
 project_folder = os.path.expanduser('./')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
@@ -9,13 +10,37 @@ client = discord.Client()
 meetings = []
 
 async def make_meeting(parameters):
-    #Assume only name
+    #Name parameter
     name = parameters[0]
-    meetings.append(schedule.Meeting(name))
+
+    #Time parameter HH:MM (24HR)
+    meeting_time = datetime.datetime.now().time #Default is now
+    if (len(parameters[1]) == 5):
+        meeting_time = datetime.time(int(parameters[1][:2]), int(parameters[1][3:]))
+
+    #Date parameter DD/MM/YYYY (if year is omitted, assumed the current)
+    meeting_date = datetime.date.today() #If the date is omitted, assume today
+    if (len(parameters[2]) == 5):
+        meeting_date = datetime.date(meeting_date.year, int(parameters[2][3:]), int(parameters[2][:2]))
+    elif (len(parameters[2]) == 10):
+        meeting_date = datetime.date(int(parameters[2][6:]), int(parameters[2][3:5]), int(parameters[2][:2]))
+
+    #Participants parameter - all the @ users
+    #TODO
+
+    #Desc parameter - string (in quotes)
+    #TODO
+
+    #Autoremind parameter - TRUE or FALSE
+    #TODO
+
+    meetings.append(schedule.Meeting(name, meeting_time, meeting_date))
 
 async def show_meetings(message):
     for meeting in meetings:
-        await message.channel.send(meeting.getName())
+        await message.channel.send(meeting.getName() + "\n" + 
+              str(meeting.getTime().hour).zfill(2) + ":" + str(meeting.getTime().minute).zfill(2) + " " + str(meeting.getDate().day).zfill(2) + 
+              "/" + str(meeting.getDate().month).zfill(2) + "/" + str(meeting.getDate().year).zfill(2))
 
 async def process_command(message):
     parameters = message.content.split(' ')
